26cb31dd6d58a13b18bd21648ecc55095e6ef039
mohammed <bchaikermed@gmail.com>
2021-01-07 11:39:24 +0100

    new

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Discussion;
+use App\Entity\Theme;
+use App\Form\DiscussionType;
+use App\Form\ThemeType;
+use App\Repository\DiscussionRepository;
+use App\Repository\ThemeRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForumController extends AbstractController
+{
+    /**
+     * @Route("/discussion", name="index_forum")
+     * @param DiscussionRepository $discussionRepository
+     * @return Response
+     */
+    public function index(DiscussionRepository $discussionRepository ): Response
+    {
+        $discussions = $discussionRepository->findAll();
+
+        return $this->render('forum/index.html.twig', [
+            'controller_name' => 'ForumController',
+            'discussions'=>$discussions
+        ]);
+    }
+
+    /**
+     * @Route("/discussion/{id}", name="show_forum")
+     * @param Discussion $discussion
+     * @return Response
+     */
+
+    public function show(Discussion $discussion){
+        return $this->render('forum/discussion.html.twig',['discussion'=>$discussion]);
+    }
+
+
+
+
+    /**
+     * @Route ("/", name="welcome")
+     * @param ThemeRepository $themeRepository
+     * @return Response
+     */
+    public function welcome(ThemeRepository $themeRepository){
+        $themes = $themeRepository->findAll();
+        return $this->render('forum/welcome.html.twig',['themes'=>$themes]);
+    }
+
+
+    /**
+     * @Route ("theme/{id}", name="theme_discussion")
+     * @param Theme $theme
+     * @return Response
+     */
+    public function show_discussions(Theme $theme){
+        $val = $theme->getDiscussions()->count();
+        return $this->render('forum/content.html.twig',['theme'=>$theme,'val'=>$val]);
+    }
+
+
+    /**
+     * @Route ("/creatediscussion", name="create_discussion")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     */
+
+
+    public function create_discussion(Request $request, ObjectManager $manager){
+       $discussion = new Discussion();
+       $form = $this->createForm(DiscussionType::class,$discussion);
+       $view = $form->createView();
+
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid()){
+           $discussion->setCreatedAt(new \DateTime());
+           $manager->persist($discussion);
+           $manager->flush();
+
+           return $this->redirectToRoute('show_forum',['id'=>$discussion->getId()]);
+
+       }
+       return $this->render('forum/addDiscussion.html.twig',['view'=>$view]);
+    }
+
+    /**
+     * @Route ("create_theme", name="create_theme")
+    */
+
+
+    public function create_theme(Request $request, ObjectManager $manager){
+        $theme = new Theme();
+        $form=$this->createForm(ThemeType::class,$theme);
+        $view=$form->createView();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $theme->setCreatedAt(new \DateTime());
+            $manager->persist($theme);
+            $manager->flush();
+            return $this->redirectToRoute('theme_discussion',['id'=>$theme->getId()]);
+        }
+        return $this->render('forum/addTheme.html.twig',['view'=>$view]);
+
+}
+
+
+}
---
+++ src/Entity/Theme.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThemeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ThemeRepository::class)
+ */
+class Theme
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Discussion::class, mappedBy="theme")
+     */
+    private $discussions;
+
+    public function __construct()
+    {
+        $this->discussions = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Discussion[]
+     */
+    public function getDiscussions(): Collection
+    {
+        return $this->discussions;
+    }
+
+    public function addDiscussion(Discussion $discussion): self
+    {
+        if (!$this->discussions->contains($discussion)) {
+            $this->discussions[] = $discussion;
+            $discussion->setTheme($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDiscussion(Discussion $discussion): self
+    {
+        if ($this->discussions->removeElement($discussion)) {
+            // set the owning side to null (unless already changed)
+            if ($discussion->getTheme() === $this) {
+                $discussion->setTheme(null);
+            }
+        }
+
+        return $this;
+    }
+}
